6a138338ed622c7d504be6365242fb1f1c8fc57d
PlanetMacro <mirco@leastauthority.com>
2025-05-12 16:02:02 +0200

    linearMvExtension

--- ArkLib/ProofSystem/Whir/ToCodingTheory/LinearMvExtension.lean
+++ ArkLib/ProofSystem/Whir/ToCodingTheory/LinearMvExtension.lean
@@ -1,51 +1,56 @@
-import Mathlib.Algebra.Polynomial.Basic
-import Mathlib.Algebra.Polynomial.Degree.Definitions
-import Mathlib.Algebra.MvPolynomial.Basic
-import Mathlib.RingTheory.MvPolynomial.Groebner
+import ArkLib.Data.CodingTheory.Basic
 
 /-! Univariate polynomials of degree < 2ᵐ can be writen as degree wise linear
     m-variate polynomials by
     ∑ aᵢ Xⁱ → ∑ aᵢ ∏ⱼ Xⱼ^(bitⱼ(i))` -/
 
+namespace LinearMvExtension
+
+variable {F : Type*} [CommSemiring F] {m : ℕ}
 
 /- Given integers `m` and `i` this computes monomial exponents
    ( σ(0), ..., σ(m-1) ) = ( bit₀(i), ..., bitₘ₋₁(i) )
    such that we can do X_0^σ(0)⬝  ⋯  ⬝ X_(m-1)^σ(m-1).
    For i ≥ 2ᵐ this is the bit reprsentation of (i mod 2ᵐ) -/
-noncomputable def bitExpo
- (m i : ℕ ): (Fin m) →₀ ℕ :=
-      Finsupp.onFinset Finset.univ
-        (fun j => if Nat.testBit i j.1 then 1 else 0)
-        (by intro j hj; simpa using hj)
+noncomputable def bitExpo (i : ℕ ): (Fin m) →₀ ℕ :=
+  Finsupp.onFinset Finset.univ
+    (fun j => if Nat.testBit i j.1 then 1 else 0)
+    (by intro j hj; simpa using hj)
 
-/-- `linMvExt m p` rewrites the univariate polynomial `p : F^(<2^m)[X]`
-     as a degree wise linear multivariate polynomial in the variables
-     `X₀ … Xₘ₋₁`, sending `aᵢ Xᶦ ↦ aᵢ ∏ⱼ Xⱼ^(bitⱼ(i))`,
-     where `bitⱼ(i)` is the `j`-th binary digit of `i`. -/
-noncomputable def linMvExt {F : Type*} [Field F]
-  (m : ℕ)
-  (p : Polynomial F) (_hp : p.degree < (2 ^ m)) : MvPolynomial (Fin m) F :=
-    (p : Polynomial F).sum fun i a =>
-      -- aᵢ Xⁱ ↦ aᵢ ∏ⱼ Xⱼ^(bitⱼ(i))
-      MvPolynomial.monomial (bitExpo m i)  a
+/-- The linear map that maps univariate polynomials of degree `< 2ᵐ` onto
+    a degree wise linear m-variate polynomial, sending
+    `aᵢ Xᶦ ↦ aᵢ ∏ⱼ Xⱼ^(bitⱼ(i))`, where `bitⱼ(i)` is the `j`-th binary digit of `i`. -/
+noncomputable def linearMvExtension:
+    Polynomial.degreeLT F (2 ^ m) →ₗ[F] MvPolynomial (Fin m) F where
+  -- p(X) = aᵢ Xᶦ ↦ aᵢ ∏ⱼ Xⱼ^(bitⱼ(i))
+  toFun p := (p : Polynomial F).sum fun i a =>
+    MvPolynomial.monomial (bitExpo i)  a
+  map_add' := by
+    rintro p q
+    simp [Polynomial.sum_add_index]
+  map_smul' := by
+    rintro c p
+    simp [Polynomial.sum_smul_index]
+    sorry
+
+/-- the linearized m-variate polynomial -/
+noncomputable def linearMvExtended
+  (p : Polynomial.degreeLT F (2 ^ m)) : MvPolynomial (Fin m) F :=
+    linearMvExtension.toFun p
 
 
-/- x → (x^(2⁰),x^(2¹),...,x^(2⁽ᵐ⁻¹⁾) -/
-noncomputable def pow {F : Type*} [Field F] (m : ℕ) : Fin m → Polynomial F :=
+/- X → (X^(2⁰),X^(2¹),...,X^(2⁽ᵐ⁻¹⁾) -/
+noncomputable def pow : Fin m → Polynomial F :=
   fun j : Fin m => (Polynomial.X : Polynomial F) ^ (2 ^ (j : ℕ))
 
-/- Maps m-variate polynomials onto an univariate polynomial by
-   invLinMvExt[p](x) := p(x^(2⁰),x^(2¹),...,x^(2⁽ᵐ⁻¹⁾))  -/
-noncomputable def leftInvLinMvExt  {F : Type*} [Field F]
-  {m : ℕ} (p: MvPolynomial (Fin m) F) : Polynomial F :=
-    (MvPolynomial.eval₂Hom
-      (Polynomial.C : F →+* Polynomial F)
-      (pow m) p
-    )
+/- For an univariate polynomial of degree < 2ᵐ, we have
+   linearMvExtension[p](X^(2⁰),X^(2¹),...,X^(2⁽ᵐ⁻¹⁾) = p(X)-/
+lemma pow_is_left_inverse (p : Polynomial.degreeLT F (2 ^ m)) :
+  MvPolynomial.eval₂
+    (Polynomial.C : F →+* Polynomial F)
+    pow
+    (linearMvExtended p)
+  = (p : Polynomial F) := sorry
 
-/- For an univariate polynomial of degree < 2ᵐ, leftInvLinMvExt is a left inverse to
-   linMvExt -/
-lemma is_left_inverse
-    {F : Type*} [Field F] {m : ℕ}
-    (p : Polynomial F) (hp : p.degree < (2 ^ m)) :
-      leftInvLinMvExt (linMvExt m p hp) = p := by sorry
+
+end LinearMvExtension
